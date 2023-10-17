e8e99c70475d564b5957f50a8baf324e4ed90803
jonghak-dev <jonghak.dev@gmail.com>
2023-10-17 14:40:55 +0900

    Add build files

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		targetDir := "/eks-share/mnt/s3"
+		files, err := os.ReadDir(targetDir)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		for _, file := range files {
+			fmt.Println(file.Name())
+		}
 		w.Write([]byte("hello"))
 	})
 	http.ListenAndServe(":8080", nil)
